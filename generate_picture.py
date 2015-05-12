1b179b6e996e7da685fd03ec25c5f880a5851988
Daan van Gent <daanvangent@gmail.com>
2015-05-12 17:42:13 +0200

    New data generated on 32-core system

--- generate_picture.py
+++ generate_picture.py
@@ -1,3 +1,5 @@
+import sys
+
 texA = """\
 \\documentclass[tikz]{standalone}
 \\usetikzlibrary{shapes.geometric}
@@ -11,7 +13,7 @@ texB = """\
     ]
 \\foreach \\jRow[count=\\jCount from 1, remember=\\mCount] in {%
 """
-c = ["/.style={fill=red!25}","/.style={fill=green!25}"]
+c = ["/.style={fill=red!25}","/.style={fill=blue!25}","/.style={fill=green!25}"]
 texC = """%
     } {
     \\foreach \\mCell[count=\\mCount from 1, remember=\\mCount] in \\jRow {
@@ -35,22 +37,37 @@ texC = """%
 \\end{document}
 """
 
-with open( "output.csv", "r") as datain:
-    with open( "output.tex", "w" ) as dataout:
+if( len( sys.argv ) != 4 ):
+    print "usage: ./generate_picture.py input output (-n|-s)" 
+    exit()
+
+with open( sys.argv[1], "r") as datain:
+    with open( sys.argv[2], "w" ) as dataout:
         n = 0
         data = []
 
         dataout.write(texA)
-        for line in datain:
-            data.append([])
-            for x in line.split():
-                data[n].append(int(x))
-            n+=1
-        print( data )
+
+        if( sys.argv[3] == "-n" ):
+            for line in datain:
+                data.append([])
+                for x in line.split():
+                    data[n].append(int(x))
+                n+=1
+            print( data )
+        else:
+            for line in datain:
+                l = line.split()
+                while( len(data) < int(l[0]) ):
+                    data.append([])
+                if( len( data[int(l[0])-1] ) == 0 ):
+                    data[int(l[0])-1] = list( 0 for i in range(0,int(l[0])))
+                data[int(l[0])-1][int(l[1])] = int(l[2])
+            n = len(data)
 
         for row in range(1,n+1):
             for cell in range(1,row+1):
-                dataout.write( "cell " + str(n-row+cell) + "-" + str(cell) + c[(data[row-1][cell-1]+1)/2] + ",\n" )
+                dataout.write( "cell " + str(n-row+cell) + "-" + str(cell) + c[data[row-1][cell-1]+1] + ",\n" )
 
 
         dataout.write(texB)
